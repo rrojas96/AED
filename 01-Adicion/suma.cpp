139424f7376a391212f11a06a08696ed80aa2770
Rodrigo Cabñas <38092677+rrojas96@users.noreply.github.com>
2018-07-12 15:42:55 -0300

    Add files via upload

---
+++ 01-Adicion/suma.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+	std::cout<<"ingrese un numero \n"
+	int a;				
+	std::cin >> a;		//Lee 1° numero
+	std::cout<<"ingrese otro numero \n"
+	int b;				
+	std::cin >> b;		//Lee 2° numero
+	std::cout<<"La suma es:" << a+b;	//Devuelvo la suma
+}
